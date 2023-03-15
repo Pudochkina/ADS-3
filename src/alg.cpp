bc3a142b2c2be94fad536aa359c1d5d86f5ef810
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-03-16 02:20:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,43 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int count = 0;
+int mid = arr[size/2];
+while (size >= 0) {
+if (value > mid) {
+arr == arr+mid+1;
+size = size - mid - 1;
+mid = arr[mid+1+size/2];
+}
+else if (value < mid) {
+mid = arr[size/4];
+size = size/2;
+}
+else if (value == mid) {
+count++;
+for (int i = 0; i < (size/2); i++){
+if (value == arr[i]){
+count++;
+}
+}
+if (value == arr[size/2+1] && value == arr[size/2-1]){
+count++;
+}
+if (value == arr[size/2+1] || value == arr[size/2-1]){
+count++;
+}
+arr == arr+mid+1;
+size = size - mid - 1;
+mid = arr[mid+1+size/2];
+if (value == arr[size/2+1]){
+count++;
+}
+}
+}
+if (count > 0) {
+return count;
+}
+else{
+return 0;
+}
 }
