92a98872af47a8fd49f214236e4f3ac74e9d0c9c
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-03-16 02:26:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,36 +8,33 @@ if (value > mid) {
 arr == arr+mid+1;
 size = size - mid - 1;
 mid = arr[mid+1+size/2];
-}
-else if (value < mid) {
+} else if (value < mid) {
 mid = arr[size/4];
 size = size/2;
-}
-else if (value == mid) {
+} else if (value == mid) {
 count++;
-for (int i = 0; i < (size/2); i++){
-if (value == arr[i]){
-count++;
-}
-}
-if (value == arr[size/2+1] && value == arr[size/2-1]){
+for (int i = 0; i < (size/2); i++) {
+if (value == arr[i]) {
 count++;
 }
-if (value == arr[size/2+1] || value == arr[size/2-1]){
+}
+if (value == arr[size/2+1] && value == arr[size/2-1]) {
+count++;
+}
+if (value == arr[size/2+1] || value == arr[size/2-1]) {
 count++;
 }
 arr == arr+mid+1;
 size = size - mid - 1;
 mid = arr[mid+1+size/2];
-if (value == arr[size/2+1]){
+if (value == arr[size/2+1]) {
 count++;
 }
 }
 }
 if (count > 0) {
 return count;
-}
-else{
+} else {
 return 0;
 }
 }
